caa17ca82cd43eb9eb92a704cd4ab4c1663765c0
Brent Danley <brentdanley@gmail.com>
2020-11-04 18:55:05 -0500

    Adds modular styles to single show component

--- src/components/single-show.js
+++ src/components/single-show.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import styles from './single-show.module.css'
 
 const SingleShow = (props) => {
   return (
-    <div>
-      <img src={props.image} alt='' />
-      <h3>{props.title}</h3>
-      <div dangerouslySetInnerHTML={{ __html: props.description }} />
+    <div className={styles.wrapper}>
+      <img className={styles.image} src={props.image} alt='' />
+      <h3 className={styles.title}>{props.title}</h3>
+      <div className={styles.description} dangerouslySetInnerHTML={{ __html: props.description }} />
+      <button className={styles.button}>Show Episodes</button>
     </div>
   )
 }
