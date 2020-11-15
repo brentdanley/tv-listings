65ce04700d6c26f07d68c3e17922480226baea33
Brent Danley <brentdanley@gmail.com>
2020-11-14 22:10:19 -0500

    Sets up a router and pulls parameter from url for show search

--- src/App.js
+++ src/App.js
@@ -1,64 +1,27 @@
 import React from 'react'
-import SingleShow from './components/single-show'
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom"
+
+import ShowSearch from './components/pages/ShowSearch'
+import SingleShow from './components/pages/SingleShow'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      shows: [],
-      searchTerm: '',
     }
-
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.onChange = this.onChange.bind(this)
-  }
-
-  handleSubmit = e => {
-    e.preventDefault()
-    fetch(`https://api.tvmaze.com/search/shows?q=${this.state.searchTerm}`)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            shows: result,
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-          });
-        }
-      )
-  }
-
-  onChange = e => {
-    this.setState({ searchTerm: e.target.value })
   }
 
   render() {
   
-    const shows = this.state.shows;
     return (
       <div>
-        <header>
-          <h1>Brent's Cool Show Listings</h1>
-          <form onSubmit={this.handleSubmit}>
-            <input placeholder="Search term"
-              value={this.state.searchTerm}
-              onChange={this.onChange}
-            ></input>
-          <button>Get shows</button>
-          </form>
-        </header>
-        <div className='show-listing'>
-        {
-          shows.map(show => (
-            <SingleShow image={show.show.image.medium} title={show.show.name} description={show.show.summary} />
-          ))
-        }
-        </div>
+        <Router>
+            <Route path='/search/shows/' children={<ShowSearch />} />
+            <Route path='/show/:show_id' children={<SingleShow />} />
+        </Router>
       </div>
     )
   }
---
+++ src/components/searchForm.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import SingleShow from './single-show'
+
+class SearchForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      shows: [],
+      searchTerm: '',
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.onChange = this.onChange.bind(this)
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    fetch(`https://api.tvmaze.com/search/shows?q=${this.state.searchTerm}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            shows: result,
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+          });
+        }
+      )
+  }
+
+  onChange = e => {
+    this.setState({ searchTerm: e.target.value })
+  }
+
+  render() {
+  
+    const shows = this.state.shows;
+    return (
+      <div>
+          <h1>Brent's Cool Show Listings</h1>
+          <form onSubmit={this.handleSubmit}>
+            <input placeholder="Search term"
+              value={this.state.searchTerm}
+              onChange={this.onChange}
+            ></input>
+          <button>Get shows</button>
+          </form>
+        <div className='show-listing'>
+        {
+          shows.map(show => (
+            <SingleShow image={show.show.image.medium} title={show.show.name} description={show.show.summary} />
+          ))
+        }
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SearchForm;
