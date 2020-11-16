2cd791b871487521eed78c75e5c9dcf961a17a34
Brent Danley <brentdanley@gmail.com>
2020-11-16 16:56:16 -0500

    Adds basic format and styles to episode items in list.

--- src/components/EpisodeListItem.js
+++ src/components/EpisodeListItem.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import styles from './EpisodeListItem.module.scss'
+
 const EpisodeListItem = (props) => {
     return (
-        <div key={props.id}>
-            <p>Name: {props.name}</p>
+        <div className={styles.wrapper} key={props.id}>
+            <p>{`S${props.season}:E${props.number} ${props.name}`}</p>
             {(props.image !== null) ? <img src={props.image.medium} alt={`${props.name} poster`} /> : ''}
-            <p>Season: {props.season}</p>
+            <div className={styles.summary} dangerouslySetInnerHTML={{ __html: props.summary}}></div>
         </div>
     )
 }
