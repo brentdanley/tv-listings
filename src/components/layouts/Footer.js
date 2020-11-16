a8e7fa4254da943f83ef5972b83a76545b7bde03
Brent Danley <brentdanley@gmail.com>
2020-11-16 13:25:08 -0500

    Adds Homepage content. Adds contact info to footer.

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -5,7 +5,14 @@ const Footer = () => {
     return (
         <footer>
             <div className={styles.wrapper}>
-                <a href='https://brentdanley.codes' target='_blank' rel='noreferrer' title='Brent Danley&quot; programming portfolio.'>Brent's Coding Portfolio</a>
+                <div>Visit <a href='https://brentdanley.codes' target='_blank' rel='noreferrer' title='Brent Danley&quot; programming portfolio.'>Brent's Coding Portfolio</a></div>
+                <div className={styles.contact}>
+                    <p>&copy; {new Date().getFullYear()}</p>
+                    <p><strong>Brent Danley</strong><br />
+                    <a href="tel:207-423-7145" title="Brent's phone number">(207) 423-7145</a><br />
+                    <a href="mailto:brentdanley@gmail.com" title="Brent's email address">brentdanley@gmail.com</a>
+                    </p>
+                </div>
             </div>
         </footer>
     )
